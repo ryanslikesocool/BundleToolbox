46cf33a607ae6edb58d6a08103ad794fd7c4652d
Ryan Boyer <ryanjboyer@gmail.com>
2024-12-27 06:54:17 -0700

    Add minimum platform versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,14 @@ import PackageDescription
 let package = Package(
 	name: "BundleToolbox",
 	defaultLocalization: "en",
+	platforms: [
+		.iOS(.v13),
+		.macCatalyst(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6),
+		.visionOS(.v1),
+	],
 	products: [
 		.library(
 			name: "BundleToolbox",
